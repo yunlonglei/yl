e727966ec44f37ca7c610cb1c866bf9ecdd310a3
leiyunlong <1175333149@qq.com>
2019-05-22 13:22:45 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @author lei
+ */
+public class Hello {
+    public static void main(String[] args) {
+        Hello.dsFdsa();
+    }
+
+    public static void dsFdsa() {
+        System.out.print("hello,world");
+    }
+}
